bd630de1d4199104cab9b54359124b83c307e72c
Vince Andrew Pimentel <contact@vincep.me>
2018-12-20 04:18:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
   ON series.id = characters.series_id
   WHERE LOWER(characters.species) = 'human'
   GROUP BY series.title
-  ORDER BY COUNT(characters.species);"
+  ORDER BY COUNT(characters.species) DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
