e9885e05958786d2a0e759a36ed89db4fb6a80cf
Vince Andrew Pimentel <contact@vincep.me>
2018-12-20 03:28:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
   FROM books
   INNER JOIN series
-  ON books.series_id = series.id
+  ON series.id = books.series_id
   ORDER BY books.year;"
 end
 
